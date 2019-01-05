37bc21427b3325af941e6917069bc2bfca873e3f
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-05 15:32:15 -0600

    Update points.js

--- points.js
+++ points.js
@@ -1,1 +1,38 @@
+function getGenMult(tier) {
+	let multi = Math.pow(1.015,player["tier"+tier].bought);
+	return multi;
+}
 
+function getProductionAmount(tier) {
+	if(tier >= 10) {
+		return 0;
+	}
+	return getGenMult(tier)*player["tier"+(tier+1)].amount;
+}
+
+function canBuyGen(tier) {
+	if(player["tier"+(tier+1)].cost <= player.points && player["tier"+(tier+1)].unlocked) {
+		return true;
+	}
+	return false;
+}
+
+function buyGen(tier) {
+	if(canBuyGen(tier)) {
+		player["tier"+tier].bought++;
+		player["tier"+tier].amount++;
+		player["tier"+tier].cost *= player["tier"+tier].costMult;
+	}
+}
+
+function buyMaxGen(tier) {
+	while(canBuyGen(tier)) {
+		buyGen(tier);
+	}
+}
+
+function maxAll() {
+	for(let i = 1; i <= 10; i++) {
+		buyMaxGen(i);
+	}
+}
