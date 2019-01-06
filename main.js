1e481d90565b8b87bc8851c9780737b023152316
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-05 20:26:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -174,12 +174,13 @@ function update() {
 	} else {
 		document.getElementById("lsButton").style.display = "none";
 	}
+	document.getElementById("lsButton").innerHTML = getLSAmount();
 	if(player.ls.prestiged > 0) {
 		document.getElementById("lineSegments").style.display = "";
 	} else {
 		document.getElementById("lineSegments").style.display = "none";
 	}
-	document.getElementById("lsAmount").innerHTML = player.ls.amount;
+	document.getElementById("LSOnPrestige").innerHTML = player.ls.amount;
 }
 function gameLoop() {
 	var newTime = new Date().getTime()
