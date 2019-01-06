59822112aea9f071b27dc58be3b2fc4d6514bb5c
lllllllllwith10ls <38187754+lllllllllwith10ls@users.noreply.github.com>
2019-01-05 19:42:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -102,7 +102,7 @@ function produce(fps) {
 }
 
 function pointReset() {
-	if(player.points > Math.pow(2,31)) {
+	if(player.points > Math.pow(2,23)) {
 		let costs = [null,10,100,1000,1e5,1e8,1e12,1e20,1e30,1e43,1e60];
 		let costMults = [null,1.15,1.18,1.21,1.24,1.27,1.3,1.33,1.36,1.39,1.42];
 		for(let i = 0; i <= 10; i++) {
@@ -118,7 +118,7 @@ function pointReset() {
 	}
 }
 function getLSAmount() {
-	return Math.floor(Math.sqrt(player.points)/Math.pow(2,31));
+	return Math.floor(Math.sqrt(player.points)/Math.pow(2,23));
 }
 
 function buyUpgrade(upgrade) {
