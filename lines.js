84d537191416434d643e113cc373a1bccb8c42de
Alex Henderson <bohroco@aol.com>
2019-01-08 16:57:28 +0100

    Update lines.js

--- lines.js
+++ lines.js
@@ -1,14 +1,16 @@
 function buyUpgrade(upgrade) {
 	if(canBuyUpgrade(upgrade)) {
 		let index = player.ls.potentialUpgrades.indexOf(upgrade);
+		let upg = index-7;
 		player.ls.amount = player.ls.amount.minus(player.ls.costs[index]);
 		player.ls.upgrades[index]++;
-    if(upgrade.substring(2)>3) {
-      player.ls.costs[index]*=10;
-    }
-    else {
-      player.ls.costs[index]*=5;
-    }
+    		if(upgrade.substring(2)>3) {
+      			player.ls.costs[index]*=10;
+   		}
+    		else {
+     	 		player.ls.costs[index]*=5;
+    		}
+		document.getElementById("T"+upg+"DoubleCount").innerHTML += 2;
 	}
 }
 function canBuyUpgrade(upgrade) {
