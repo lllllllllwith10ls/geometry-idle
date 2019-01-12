6db50018a8ff680d6f384e16b7b46074b74ad42a
Alex Henderson <bohroco@aol.com>
2019-01-12 14:48:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -262,7 +262,7 @@ function update() {
 		}
 	}
 	for (let i = 1; i < player.ls.potentialUpgrades.length; i++) {
-		if (i > 8) {
+		if (i > 8 && i < 18) {
 			let num = i - 7;
 			document.getElementById("T"+num+"DoubleCost").innerHTML = player.ls.costs[i];
 			document.getElementById("T"+num+"DoubleCount").innerHTML = player.ls.upgrades[i]*2;
