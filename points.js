652cec9e8b6424988e79401180c7a0567272dbe4
Alex Henderson <bohroco@aol.com>
2019-01-12 08:09:55 +0100

    Update points.js

--- points.js
+++ points.js
@@ -1,10 +1,18 @@
 function getGenMult(tier) {
 	let multi = new Decimal(1.015).pow(player["tier"+tier].bought);
-	let upgrade = player.ls.upgrades[7+tier];
-	if (upgrade>0) {
+	let lineUpgrade = player.ls.upgrades[7+tier];
+	if (lineUpgrade>0) {
 		multi = multi.times(upgrade*2);
 	}
+	let dotBonus = player.dotTier1.amount.sqrt().plus(1);
+	multi = multi.times(dotBonus);
 	player["tier"+tier].multiplier = multi;
+	return multi;
+}
+
+function getDotMult(tier) {
+	let multi = new Decimal(1.03).pow(player["dotTier"+tier].bought);
+	player["dotTier"+tier].multiplier = multi;
 	return multi;
 }
 
@@ -15,8 +23,22 @@ function getProductionAmount(tier) {
 	return getGenMult(tier).times(player["tier"+tier].amount);
 }
 
+function getDotProductionAmount(tier) {
+	if(tier > 10) {
+		return new Decimal(0);
+	}
+	return getDotMult(tier).times(player["dotTier"+tier].amount);
+}
+
 function canBuyGen(tier) {
 	if(player["tier"+tier].cost.lte(player.points) && player["tier"+tier].unlocked) {
+		return true;
+	}
+	return false;
+}
+
+function canBuyDot(tier) {
+	if(player["dotTier"+tier].cost.lte(player.ls.amount) && player["dotTier"+tier].unlocked) {
 		return true;
 	}
 	return false;
@@ -32,6 +54,21 @@ function buyGen(tier) {
 	}
 }
 
+function buyDot(tier) {
+	if(canBuyDot(tier)) {
+		player["dotTier"+tier].bought++;
+		player["dotTier"+tier].amount = player["dotTier"+tier].amount.plus(1);
+		player.points = player.points.minus(player["dotTier"+tier].cost);
+		player["dotTier"+tier].cost = player["dotTier"+tier].cost.times(player["dotTier"+tier].costMult);
+	}
+}
+
+function buyMaxDot(tier) {
+	while(canBuyDot(tier)) {
+		buyDot(tier);
+	}
+}
+
 function buyMaxGen(tier) {
 	while(canBuyGen(tier)) {
 		buyGen(tier);
@@ -44,3 +81,8 @@ function maxAll() {
 	}
 }
 
+function maxallDots() {
+	for(let i = 10; i >= 1; i--) {
+		buyMaxDot(i);
+	}
+}
