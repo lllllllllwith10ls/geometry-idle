a91b9477800069ab0d110ebe5f89e256ce668bd1
Alex Henderson <bohroco@aol.com>
2019-01-12 14:33:00 +0100

    Update lineSegments.js

--- lineSegments.js
+++ lineSegments.js
@@ -13,12 +13,11 @@ function buyUpgrade(upgrade) {
 			let upg = index-7;
     			if(upgrade.substring(2)>3) {
       				player.ls.costs[index] *= 10;
-				document.getElementById("T"+upg+"DoubleCost").innerHTML = player.ls.costs[index];
    			}
     			else {
      	 			player.ls.costs[index] *= 5;
-				document.getElementById("T"+upg+"DoubleCost").innerHTML = player.ls.costs[index];
     			}
+			document.getElementById("T"+upg+"DoubleCost").innerHTML = player.ls.costs[index];
 			document.getElementById("T"+upg+"DoubleCount").innerHTML = player.ls.upgrades[index]*2;
 		}
 		else {
