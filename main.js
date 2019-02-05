7be589af9865daa7d06d41bccf2d6ba197018e42
Alex Henderson <bohroco@aol.com>
2019-02-05 13:18:48 +0100

    Added COMMENTS
    Like WUT? Responsible coding???

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-function getDefaultSave() { //Create a new 
+function getDefaultSave() { //Create a new Player with base values
 	return {
-		points: new Decimal(0),
-		dots: new Decimal(0),
-		tier1: {
+		points: new Decimal(0), //Zero points
+		dots: new Decimal(0), //Zero dots
+		tier1: { //First Point Generator
 			cost: new Decimal(10),
 			amount: new Decimal(0),
 			multiplier: new Decimal(1),
 			bought: 0,
 			costMult: new Decimal(1.15),
-			costRaise: 1.01,
+			costRaise: 1.01, //Cost Raise affects Cost Mult above 100 Generators
 			unlocked: true
 		},
-		tier2: {
+		tier2: { //Second Point Generator
 			cost: new Decimal(100),
 			amount: new Decimal(0),
 			multiplier: new Decimal(1),
 			bought: 0,
 			costMult: new Decimal(1.18),
-			costRaise: 1.012,
+			costRaise: 1.012, 
 			unlocked: true
 		},
-		tier3: {
+		tier3: { //...and so on
 			cost: new Decimal(1e3),
 			amount: new Decimal(0),
 			multiplier: new Decimal(1),
@@ -29,14 +29,14 @@ function getDefaultSave() { //Create a new
 			costRaise: 1.014,
 			unlocked: true
 		},
-		ls: {
-			amount: new Decimal(0),
+		ls: { //Line Segments Information
+			amount: new Decimal(0), //All upgrades, costs, and times purchased
 			potentialUpgrades: ["MU","T4","T5","T6","T7","T8","T9","T10","DT1","DT2","DT3","DT4","DT5","DT6","DT7","DT8","DT9","DT10","DG"],
 			costs:             [1   ,60  ,70  ,80  ,90  ,100 ,110 ,120  ,10   ,10   ,10   ,15   ,20   ,25   ,30   ,35   ,40   ,45    ,200 ],
 			upgrades: 	   [0   ,0   ,0   ,0   ,0   ,0   ,0   ,0    ,0    ,0    ,0    ,0    ,0    ,0    ,0    ,0    ,0    ,0     ,0   ],
-			prestiged: 0
+			prestiged: 0 //How many times you have completed a Linear Raise. This resets on Linear Extension
 		},
-		tier4: {
+		tier4: { //Back to Point Generators
 			cost: new Decimal(1e5),
 			amount: new Decimal(0),
 			multiplier: new Decimal(1),
@@ -99,12 +99,12 @@ function getDefaultSave() { //Create a new
 			costRaise: 1.028,
 			unlocked: false
 		},
-		dotTier1: {
+		dotTier1: { //Now for Dot Generators!
 			cost: new Decimal(10),
 			amount: new Decimal(0),
 			multiplier: new Decimal(1),
 			bought: 0,
-			costMult: new Decimal(1.12),
+			costMult: new Decimal(1.12), //They don't have a Cost Raise. This might change in the future
 			unlocked: true
 		},
 		dotTier2: {
@@ -179,27 +179,27 @@ function getDefaultSave() { //Create a new
 			costMult: new Decimal(1.30),
 			unlocked: false
 		},
-		lines: {
-			amount: new Decimal(0),
+		lines: { //Line information
+			amount: new Decimal(0), //Potantial upgrades list with upgrades and purchase amount
 			potentialUpgrades: ["11", "12", "21", "31", "41", "51", "61", "71", "81", "91", "101", "111"],
 			upgrades: 	   [   0,    0,    0,    0,    0,    0,    0,    0,    0,    0,     0,     0],
 			costs: 		   [   1,    1,   12,   50,  250,  200,  400,  800, 1600, 3200,  6400, 12800],
-			prestiged: 0
+			prestiged: 0 //Number of times completed a Linear Extension
 		},
-		currentTab: "generators",
-		notation: "Standard",
-		lastTick: new Date().getTime(),
-		version: 0
+		currentTab: "generators", //Used to keep the tab the same on reloading a saved game
+		notation: "Standard", //Used to keep the player's notation the same
+		lastTick: new Date().getTime(), //Initializing the time the player was created
+		version: 0 //Uhhh... version... I guess
 	};
 }
-let player = getDefaultSave();
+let player = getDefaultSave(); //If there is no player, it's a new player!
 
-for (let i = 1; i < player.ls.costs.length; i++) {
+for (let i = 1; i < player.ls.costs.length; i++) { //This might be useless. Converting all costs to Decimal form for ease of computing
 	player.ls.costs[i] = new Decimal(player.ls.costs[i]);
 }
 
-function produce(offline) {
-	if(player.points == Number.NaN) {
+function produce(offline) { //Produce all dots, points, and generators based on the time difference
+	if(player.points == Number.NaN) { //This is me hoping I fixed the Infinity bug.
 		player.points = getProductionAmount(1).times(offline);
 	}
 	else {
@@ -213,10 +213,10 @@ function produce(offline) {
 	update();
 }
 
-function pointReset() {
+function pointReset() { //Doing a Linear Raise resets all progress before Line Segments
 	if(player.points.gte(Math.pow(2,23/2))) {
 		document.getElementById("lineSegTab").style.display="block";
-		player.ls.amount = player.ls.amount.add(getLSAmount());
+		player.ls.amount = player.ls.amount.add(getLSAmount()); //Upgrades can affect the LS Amount recieved
 		let costs = [null,10,100,1000,1e5,1e8,1e12,1e20,1e30,1e43,1e60];
 		let costMults = [null,1.15,1.18,1.21,1.24,1.27,1.3,1.33,1.36,1.39,1.42];
 		for(let i = 1; i <= 10; i++) {
@@ -230,11 +230,11 @@ function pointReset() {
 		player.ls.prestiged++;
 	}
 }
-function getLSAmount() {
-	if(player.lines.upgrades[0] > 0){
+function getLSAmount() { //Finding out how many Line Segments you will get on Linear Raise
+	if(player.lines.upgrades[0] > 0){ //This is the triple LS upgrade
 		if(player.points.root(11.5).divide(4).gte(1)) {
 			let x = new Decimal(1);
-			if(player.lines.upgrades[4] > 0) {
+			if(player.lines.upgrades[4] > 0) { //This is the LS boost based on total generators purchased
 				for(let i = 1; i < 11; i++) {
 					let y = new Decimal(0.001);
 					x = x.plus(y.times(player["tier"+i].bought));
@@ -242,15 +242,15 @@ function getLSAmount() {
 			}
 			document.getElementById("41Bonus").innerHTML = formatValue(player.notation, x, 3, 3);
 			return player.points.root(11.5).divide(4).times(3).times(x).floor();
-		} else {
+		} else { //This is to keep the first LS gained after the triple upgrade from being at like 12 points or something
 			return new Decimal(0);
 		}
-	} else {		
+	} else { //If no upgrades, then progress with the usual formula (around 8.7 mllion points)
 		return player.points.root(11.5).divide(4).floor();
 	}
 }
 
-function showTiers() {
+function showTiers() { //If you've unlocked a tier, it will be shown. Else it will be hidden. Need to add Dot Tiers here.
 	for (let i = 2; i <= 10; i++) {
 		if (player["tier" + (i - 1)].amount.gte(0) && player["tier" + i].unlocked) {
 			document.getElementById("row" + i).style.display = "";
@@ -259,12 +259,12 @@ function showTiers() {
 		}
 	}
 	for(let i = 4; i <= 10; i++) {
-		if(player.ls.upgrades[i-3] == 1) {
+		if(player.ls.upgrades[i-3] == 1) { //Checking the unlock upgrades
 			player["tier"+i].unlocked = true;
 		}
 	}
 }
-function showTab(tabName) {
+function showTab(tabName) { //Tab switching function
 	var tabs = document.getElementsByClassName('tab');
 	var tab;
 	for (var i = 0; i < tabs.length; i++) {
@@ -276,11 +276,11 @@ function showTab(tabName) {
 		else tab.style.display = 'none';
 	}
 }
-function update() {
+function update() { //This is SUCH an ugly function. I will split it up into sections soon for readability
 	showTiers();
-	document.getElementById("points").innerHTML = formatValue(player.notation, player.points, 3, 0);
-	document.getElementById("dots").innerHTML = formatValue(player.notation, player.dots, 3, 0);
-	for (let i = 1; i <= 10; i++) {
+	document.getElementById("points").innerHTML = formatValue(player.notation, player.points, 3, 0); //Clean up point amount
+	document.getElementById("dots").innerHTML = formatValue(player.notation, player.dots, 3, 0); //Clean up dot amount
+	for (let i = 1; i <= 10; i++) { //Clean up all values for each tier and dot tier
 		let str = "tier" + i + "Amount";
 		let dstr = "dotTier" + i + "Amount";
 		document.getElementById(str).innerHTML = formatValue(player.notation, player["tier" + i].amount, 3, 0);
@@ -289,7 +289,7 @@ function update() {
 		document.getElementById("buyDot" + i).innerHTML = "Cost: " + formatValue(player.notation, player["dotTier" + i].cost, 3, 0) + " line segments";
 		document.getElementById("mult" + i).innerHTML = "x" + formatValue(player.notation, getGenMult(i), 3, 3);
 		document.getElementById("dotMult" + i).innerHTML = "x" + formatValue(player.notation, getDotMult(i),3, 3);
-		if(canBuyGen(i)) {
+		if(canBuyGen(i)) { //If you can buy a generator, the button will change
 			document.getElementById("buy" + i).className = "button";
 			document.getElementById("buy" + i + "Max").className = "button";
 		} else {
@@ -304,12 +304,12 @@ function update() {
 			document.getElementById("buyDot" + i + "Max").className = "buttonlocked";
 		}
 	}
-	for (let i = 8; i < 18; i++) {
+	for (let i = 8; i < 18; i++) { //Clean up upgrade costs
 		let num = i - 7;
 		document.getElementById("T"+num+"DoubleCost").innerHTML = formatValue(player.notation, new Decimal(player.ls.costs[i]), 3, 0)
 		document.getElementById("T"+num+"DoubleCount").innerHTML = formatValue(player.notation, new Decimal(Math.pow(2, player.ls.upgrades[i])), 3, 0);
 	}
-	for (let i = 1; i < player.ls.potentialUpgrades.length; i++) {
+	for (let i = 1; i < player.ls.potentialUpgrades.length; i++) { //Change colors for LS Upgrade buttons
 		let name = player.ls.potentialUpgrades[i];
 		if (player.ls.amount.gte(player.ls.costs[i])) {
 			document.getElementById(name).style.background = "#FFFFFF";
@@ -318,7 +318,7 @@ function update() {
 			document.getElementById(name).style.background = "#A3A3A3";
 		}
 	}
-	for (let i = 1; i < 8; i++) {
+	for (let i = 1; i < 8; i++) { //If you've purchased a point generator tier, the button will vanish
 		let num = i + 3;
 		if (player.ls.upgrades[i] > 0) {
 			document.getElementById("T"+num).style.display = "none";
@@ -329,7 +329,7 @@ function update() {
 			document.getElementById("DT"+num).style.display = "none";
 		}
 	}
-	for (let i = 1; i < 10; i++) {
+	for (let i = 1; i < 10; i++) { //Showing each new dot tier when you have 10 of the prior, and the new one is unlocked
 		let j = i + 1;
 		if(player["dotTier"+i].amount.gte(10) && player["dotTier"+j].unlocked) {
 			document.getElementById("dotRow"+j).style.display = "";
@@ -338,7 +338,7 @@ function update() {
 			document.getElementById("dotRow"+j).style.display = "none";
 		}
 	}
-	for (let i = 0; i < 5; i++) {
+	for (let i = 0; i < 5; i++) { //Changing Line Tree upgrade colors
 		if(canBuyTreeItem(player.lines.potentialUpgrades[i])) {
 			document.getElementById(player.lines.potentialUpgrades[i]).style.background = "#FFFFFF";
 		} else {
@@ -349,33 +349,33 @@ function update() {
 			document.getElementById(player.lines.potentialUpgrades[i]).style.color = "#FFFFFF";
 		}
 	}
-	if(getLSAmount().gte(1)) {
+	if(getLSAmount().gte(1)) { //Show and hide the Line Segment button
 		document.getElementById("lsButton").style.display = "";
 	} else {
 		document.getElementById("lsButton").style.display = "none";
 	}
 	document.getElementById("LSOnPrestige").innerHTML = formatValue(player.notation, getLSAmount(), 3, 0);
-	if(player.ls.prestiged > 0) {
+	if(player.ls.prestiged > 0) { //Once you've done a Linear Raise at least once, the LS Tab is shown
 		document.getElementById("lineSegments").style.display = "";
 		document.getElementById("lineSegTab").style.display="";
 	} else {
 		document.getElementById("lineSegments").style.display = "none";
 		document.getElementById("lineSegTab").style.display="none";
 	}
-	if(getLAmount().gte(1)) {
+	if(getLAmount().gte(1)) { //Show and hide the Line button
 		document.getElementById("lButton").style.display = "";
 	} else {
 		document.getElementById("lButton").style.display = "none";
 	}
 	document.getElementById("LOnPrestige").innerHTML = formatValue(player.notation, getLAmount(), 3, 0);
-	if(player.lines.prestiged > 0) {
+	if(player.lines.prestiged > 0) { //Once you've done a Linear Extension, the Line Tab is shown
 		document.getElementById("lines").style.display = "";
 		document.getElementById("lineTab").style.display = "";
 	} else {
 		document.getElementById("lines").style.display = "none";
 		document.getElementById("lineTab").style.display = "none";
 	}
-	if(player.ls.upgrades[18] > 0) {
+	if(player.ls.upgrades[18] > 0) { //Once you purchase Dot Generators, the Dot Generator Tab is shown
 		document.getElementById("dotTab").style.display="";
 		document.getElementById("DG").style.display = "none";
 		document.getElementById("DG").style.background = "#A3A3A3";
@@ -386,7 +386,7 @@ function update() {
 	document.getElementById("lsAmount").innerHTML = formatValue(player.notation, player.ls.amount, 3, 0);
 	document.getElementById("lAmount").innerHTML = formatValue(player.notation, player.lines.amount, 3, 0);
 }
-function clone(obj) {
+function clone(obj) { //Handy Dandy clone function to get a copy of a layered object
 	    var copy;
     	// Handle the 3 simple types, and null or undefined
     	if (null == obj || "object" != typeof obj) return obj;
@@ -414,14 +414,14 @@ function clone(obj) {
 	}
     throw new Error("Unable to copy obj! Its type isn't supported.");
 }
-function save(){
+function save(){ //Save the game in local storage
   	localStorage.setItem("geometryIdleSave",JSON.stringify(saveToString(player)));
 	//Right here is just some code that allows a popup message when the game saves.
   	//document.getElementById("savedInfo").style.display="inline";
   	//function foo() {document.getElementById("savedInfo").style.display="none"}
   	//setTimeout(foo, 2000);
 }
-function load() {
+function load() { //Get the game from local storage if possible
   	if (localStorage.getItem("geometryIdleSave") == null) {
 		player = getDefaultSave();
   	}
@@ -431,7 +431,7 @@ function load() {
 	}
   	return player;
 }
-function saveToString(save) {
+function saveToString(save) { //Convert each Decimal to a string for easy JSON stringification
 	var copy = clone(save);
 	var keySet = Object.keys(save);
 	for (var i = 0; i < keySet.length; i++){
@@ -444,39 +444,39 @@ function saveToString(save) {
 	}
 	return copy;
 }
-function stringToSave(newSave, base) {
+function stringToSave(newSave, base) { //Compares a JSON stringify value to the default save.
 	var keySet = Object.keys(base);
 	for (var i = 0; i < keySet.length; i++){
-		if(!newSave.hasOwnProperty(keySet[i])) {
-			newSave[keySet[i]] = base[keySet[i]];
-		}
+		if(!newSave.hasOwnProperty(keySet[i])) { //If the default save has a value not present in the player save
+			newSave[keySet[i]] = base[keySet[i]]; //aka, if something was added to the game,
+		}					// it is added to the player save with default values.
 		else {
-			if(base[keySet[i]] instanceof Decimal) {
-				newSave[keySet[i]] = new Decimal(newSave[keySet[i]]);
+			if(base[keySet[i]] instanceof Decimal) { //If the default save says a value should be a Decimal object
+				newSave[keySet[i]] = new Decimal(newSave[keySet[i]]); //make a new Decimal with that value
 			}
-			else if(Object.keys(newSave[keySet[i]]).length > 1) {
+			else if(Object.keys(newSave[keySet[i]]).length > 1) { //If a value is itself an object, recursion!
 				newSave[keySet[i]] = stringToSave(newSave[keySet[i]], base[keySet[i]]);
 			}
-		}
-	}
+		} //If a value is not supposed to be a Decimal, then it will have converted with JSON.stringify() just fine
+	}		//and we don't need to take any further action.
 	return newSave;
 }
-function exportSave() {
-	var tempInput = document.createElement("input");
-	tempInput.style = "position: absolute; left: -1000px; top: -1000px";
-	tempInput.value = JSON.stringify(saveToString(player));
-	document.body.appendChild(tempInput);
-	tempInput.select();
-	document.execCommand("copy");
-	document.body.removeChild(tempInput);
-	alert("Save copied to clipboard");
+function exportSave() { //Saving something to the clipboard is a Mess.
+	var tempInput = document.createElement("input"); //You have to create a new document element
+	tempInput.style = "position: absolute; left: -1000px; top: -1000px"; //Say it's out of the window view
+	tempInput.value = JSON.stringify(saveToString(player)); //Fill it with the player save file
+	document.body.appendChild(tempInput); //Stick the window on the main document
+	tempInput.select(); //Select the window
+	document.execCommand("copy"); //Stick the contents of said window into the clipboard
+	document.body.removeChild(tempInput); //Delete the go-between window
+	alert("Save copied to clipboard"); //Tell the player it all worked
 }
-function importSave() {
+function importSave() { //Allow the player to import a save file. This is also where "secret codes" will go.
 	var imp = prompt("Paste your save file here");
 	if(imp==null) alert("That save file doesn't work, sorry.");
 	else player = stringToSave(JSON.parse(imp), getDefaultSave());
 }
-function clearSave() {
+function clearSave() { //Deletes the player save and clears the local storage.
 	if (confirm("This is not reversible. Delete your save file?")) {
 		localStorage.removeItem("geometryIdleSave");
 		player = getDefaultSave();
@@ -507,17 +507,17 @@ function saveFixer() { //Anytime we change something that's already implemented.
 	}
 	player.lines.costs = getDefaultSave().lines.costs;
 }
-function displaySave() {
+function displaySave() { //For those who have problems with the clipboard, like on mobile devices
 	document.getElementById("savePlace").innerHTML = JSON.stringify(saveToString(player));
-	document.getElementById("displayedSave").style.display = "";
+	document.getElementById("displayedSave").style.display = ""; //This will display the save for easy copying
 }
-function gameLoop() {
+function gameLoop() { //Main gameplay function
 	let newTime = new Date().getTime()
 	let diff = (newTime - player.lastTick) / 1000;
 	player.lastTick = newTime;
 	produce(diff);
 }
-function startInterval() {
+function startInterval() { //On loading the webpage, this all is done.
 	load();
 	saveFixer();
 	showTab(player.currentTab);
